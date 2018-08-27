68eeb64e761c372a5ff4313d8da11f6b2cbda152
Valentino Pezzano <valentino_pezzano-github@yahoo.com>
2018-08-27 14:06:08 +0200

    Added example with legacy interfaces.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,6 +10,7 @@ import javax.naming.NamingException;
 
 import model.Book;
 import model.BookLocal;
+import model.BookLocalLegacy;
 import model.BookRemote;
 
 public class Main {
@@ -21,18 +22,29 @@ public class Main {
 	 * "https://docs.oracle.com/cd/E11035_01/workshop102/ejb/session/conGettingStartedWithSessionBeans.html"
 	 */
 	public static void main(String[] args) throws NamingException {
+		// Initialize embedded container
 		Map<String, Object> properties = new HashMap<>();
 		properties.put(EJBContainer.MODULES, new File("target/classes"));
 		EJBContainer ec = EJBContainer.createEJBContainer(properties);
 		Context ctx = ec.getContext();
-		// The interface BookLocal is to be used for clients running inside the
-		// container. In this case, arguments to methods can be passed by reference.
+
+		// The interface BookLocal is to be used for clients running in the same VM as
+		// the container. In this case, arguments to methods can be passed by reference
 		BookLocal bookLocal = (BookLocal) ctx.lookup("java:global/classes/BookEJB!model.BookLocal");
 		Book book = new Book("Java 8", 50f, "Java 8 main features", "1234-ABCD", 300, true);
 		bookLocal.createBook(book);
 		System.out.println("Found: " + bookLocal.findBookById(book.getId()));
+
+		// The interface BookRemote should be used for clients running in a different VM
 		BookRemote bookRemote = (BookRemote) ctx.lookup("java:global/classes/BookEJB!model.BookRemote");
 		System.out.println("Remote message: " + bookRemote.getRemoteMessage());
+
+		// In case we are dealing with legacy interfaces which we can't change,
+		// no annotations will be present in the interfaces
+		BookLocalLegacy bookLocalLegacy = (BookLocalLegacy) ctx
+				.lookup("java:global/classes/BookEJBLegacy!model.BookLocalLegacy");
+		System.out.println("Using legacy interfaces, found: " + bookLocalLegacy.findBookById(book.getId()));
+		
 		System.exit(0);
 	}
 }
--- src/main/java/main/RemoteMain.java
+++ src/main/java/main/RemoteMain.java
@@ -9,15 +9,27 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import model.BookRemote;
+import model.BookRemoteLegacy;
 
 public class RemoteMain {
 	public static void main(String[] args) throws FileNotFoundException, IOException, NamingException {
-		try(FileInputStream jndi = new FileInputStream("jndi.properties")) {
+		try (FileInputStream jndi = new FileInputStream("jndi.properties")) {
 			Properties props = new Properties();
 			props.load(jndi);
 			InitialContext ctx = new InitialContext(props);
-			BookRemote bookRemote = (BookRemote) ctx.lookup("model.BookRemote");
-			System.out.println(bookRemote.getRemoteMessage());
+			BookRemote bookRemote = (BookRemote) ctx.lookup("java:global/chapter07/BookEJB!model.BookRemote");
+			System.out.println("Using portable JNDI name: " + bookRemote.getRemoteMessage());
+
+			// The following uses a non portable JNDI name,
+			// Glassfish-specific. Favor the previous one
+			BookRemote bookRemoteNonPortable = (BookRemote) ctx.lookup("model.BookRemote");
+			System.out.println("Using non portable JNDI name: " + bookRemoteNonPortable.getRemoteMessage());
+
+			// In case we are dealing with legacy interfaces which we can't change,
+			// no annotations will be present in the interfaces
+			BookRemoteLegacy bookRemoteLegacy = (BookRemoteLegacy) ctx
+					.lookup("java:global/chapter07/BookEJBLegacy!model.BookRemoteLegacy");
+			System.out.println("Using legacy interfaces: " + bookRemoteLegacy.getRemoteMessage());
 		}
 	}
 }
